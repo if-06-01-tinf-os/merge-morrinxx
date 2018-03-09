0c3b03ac594b4c4680e5dcf0e02f38542772dd21
Marcel <31956953+morrinxx@users.noreply.github.com>
2018-03-09 12:53:50 +0100

    Merge sort

---
+++ merge-morrinxx-master/merge.c
@@ -0,0 +1,74 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+#define MAX_ARRAY_SIZE 64
+
+void read_array(int* array, int n);
+void merge(int* first_array, int n_first, int* last_array, int n_last, int* merged_array);
+void print_array(int* array, int n);
+
+int main(int argc, char* argv[])
+{
+  int test_case_count;
+  scanf("%d", &test_case_count);
+  for (int i = 0; i < test_case_count; i++) {
+    int first_array[MAX_ARRAY_SIZE];
+    int second_array[MAX_ARRAY_SIZE];
+    int merged_array[MAX_ARRAY_SIZE];
+
+    int n_first;
+    scanf("%d", &n_first);
+    read_array(first_array, n_first);
+
+    int n_second;
+    scanf("%d", &n_second);
+    read_array(second_array, n_second);
+    merge(first_array, n_first, second_array, n_second, merged_array);
+    print_array(merged_array, n_first + n_second);
+  }
+}
+
+void read_array(int* array, int n)
+{
+  for (int i = 0; i < n; i++) {
+    scanf("%d", &(array[i]));
+  }
+}
+
+void print_array(int* array, int n)
+{
+  for (int i = 0; i < n; i++) {
+    printf("%d ", array[i]);
+  }
+  printf("\n");
+}
+
+void merge(int* first_array, int n_first, int* second_array, int n_second, int* merged_array)
+{
+  int l = 0;
+  int r = 0;
+  int n = 0;
+  while(r < n_second && l < n_first){
+    if(first_array[l] > second_array[r]){
+      merged_array[n] = second_array[r];
+      r++;
+      n++;
+    }
+    else{
+      merged_array[n] = first_array[l];
+      n++;
+      l++;
+    }
+  }
+
+  while (l < n_first) {
+    merged_array[n] = first_array[l];
+    n++;
+    l++;
+  }
+  while (r < n_second) {
+    merged_array[n] = second_array[r];
+    n++;
+    r++;
+  }
+}
